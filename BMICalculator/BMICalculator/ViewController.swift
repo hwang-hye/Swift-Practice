8292842faeb95dda06a9d94becbd6c8d6bbabe3b
hwanghye <hwang.sine@gmail.com>
2024-05-22 23:51:34 +0900

    [Feat] BMI 닉네임, 리셋 버튼 UI 추가

--- BMICalculator/BMICalculator/ViewController.swift
+++ BMICalculator/BMICalculator/ViewController.swift
@@ -9,19 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
+    // UILabel
     @IBOutlet var bmiTitleLabel: UILabel!
     @IBOutlet var bmiTextLabel: UILabel!
     
-    @IBOutlet var bmiImageView: UIImageView!
-    
+    @IBOutlet var nicknameLabel: UILabel!
     @IBOutlet var heightLabel: UILabel!
     @IBOutlet var weightLabel: UILabel!
     
+    // ImageView
+    @IBOutlet var bmiImageView: UIImageView!
     
+    // UITextField
+    @IBOutlet var nicknameTextField: UITextField!
     @IBOutlet var heightTextField: UITextField!
     @IBOutlet var weightTextField: UITextField!
     
+    // UIButton
+    @IBOutlet var resetButton: UIButton!
     @IBOutlet var randomCalulateButton: UIButton!
     
     @IBOutlet var resultButton: UIButton!
@@ -34,10 +39,13 @@ class ViewController: UIViewController {
         designedLabelUI(bmiTitleLabel, text: "BMI Calculator", fontSize: 24, weight: .heavy)
         designedLabelUI(bmiTextLabel, text: "당신의 BMI 지수를\n알려드릴게요", fontSize: 15, weight: .light)
         bmiTextLabel.numberOfLines = 2
+        
+        designedLabelUI(nicknameLabel, text: "닉네임을 입력해주세요", fontSize: 15, weight: .medium)
         designedLabelUI(heightLabel, text: "키가 어떻게 되시나요?", fontSize: 15, weight: .medium)
-        designedLabelUI(weightLabel, text: "몸무게가 어떻게 되시나요?", fontSize: 15, weight: .regular)
+        designedLabelUI(weightLabel, text: "몸무게가 어떻게 되시나요?", fontSize: 15, weight: .medium)
         
         // Button UI Setting
+        designedButton(resetButton, text: "리셋하기", fontSize: 12, textColor: .red)
         designedButton(randomCalulateButton, text: "랜덤으로 BMI 계산하기", fontSize: 12, textColor: .purple)
         
         designedButton(resultButton, text: "결과 확인", fontSize: 20, textColor: .white, backgroundColor: .purple, radius: 8)
@@ -46,6 +54,7 @@ class ViewController: UIViewController {
         bmiImageView.image = UIImage(named: "image")
         
         // Text Field Setting
+        designedTextField(nicknameTextField, keyBoardType: .default)
         designedTextField(heightTextField, keyBoardType: .numberPad)
         designedTextField(weightTextField, keyBoardType: .numberPad)
 
