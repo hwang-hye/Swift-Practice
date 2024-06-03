bd275d4671358e17cb72062e02c5a1f9815f3bc5
hwanghye <hwang.sine@gmail.com>
2024-06-03 17:47:02 +0900

    [Feat] TopCity Filter 기능 추가

--- TravelMagazine/TravelMagazine/TopCity/TopCityViewController.swift
+++ TravelMagazine/TravelMagazine/TopCity/TopCityViewController.swift
@@ -7,14 +7,14 @@
 
 import UIKit
 
-class TopCityViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
+class TopCityViewController: UIViewController {
     
     @IBOutlet var topCityNavigationTiltle: UINavigationItem!
     @IBOutlet var topCityTableView: UITableView!
-    @IBOutlet var topCItySegController: UISegmentedControl!
+    @IBOutlet var topCitySegController: UISegmentedControl!
     
-    let list = CityInfo.city
+    var totalList: [City] = CityInfo.city // 전체 데이터 리스트
+    var filteredList: [City] = [] // 필터링된 데이터 리스트
     
 
     override func viewDidLoad() {
@@ -22,31 +22,59 @@ class TopCityViewController: UIViewController, UITableViewDelegate, UITableViewD
         
         topCityNavigationTiltle.title = "인기 도시"
         topCityTableView.rowHeight = 160
-        topCItySegController.setTitle("모두", forSegmentAt: 0)
-        topCItySegController.setTitle("국내", forSegmentAt: 1)
-        topCItySegController.setTitle("해외", forSegmentAt: 2)
+        topCitySegController.setTitle("모두", forSegmentAt: 0)
+        topCitySegController.setTitle("국내", forSegmentAt: 1)
+        topCitySegController.setTitle("해외", forSegmentAt: 2)
         
         topCityTableView.delegate = self
         topCityTableView.dataSource = self
         
         let xib = UINib(nibName: TopCityTableViewCell.identifier, bundle: nil)
         topCityTableView.register(xib, forCellReuseIdentifier: TopCityTableViewCell.identifier)
-
+        
+        configureSegmentedControl()
+        filterData()
     }
+    
+    func configureSegmentedControl() {
+        topCitySegController.addTarget(self, action: #selector(segmentedControlValueChanged), for: .valueChanged)
+    }
+    
+    @objc
+    func segmentedControlValueChanged(_ sender: UISegmentedControl) {
+        filterData()
+    }
+
+    func filterData() {
+        switch topCitySegController.selectedSegmentIndex {
+        case 1:
+            // Domestic
+            filteredList = totalList.filter { $0.domestic_travel }
+        case 2:
+            // International
+            filteredList = totalList.filter { !$0.domestic_travel }
+        default:
+            // All
+            filteredList = totalList
+        }
+        topCityTableView.reloadData()
+    }
+}
+
+
+extension TopCityViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return list.count
-    }
+            return filteredList.count
+        }
+    
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         let cell = tableView.dequeueReusableCell(withIdentifier: TopCityTableViewCell.identifier, for: indexPath) as! TopCityTableViewCell
         
-        cell.configureCell(list[indexPath.row])
+        let data = filteredList[indexPath.row]
+        cell.configureCell(data)
         
         return cell
     }
-
-
 }
