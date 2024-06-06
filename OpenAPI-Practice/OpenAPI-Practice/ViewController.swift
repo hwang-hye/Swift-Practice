de8e40df2a819d8f82141bf5d8b1ac21edabcfb0
hwanghye <hwang.sine@gmail.com>
2024-06-06 17:23:45 +0900

    OpenAPI-Practice init

---
+++ OpenAPI-Practice/OpenAPI-Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenAPI-Practice
+//
+//  Created by hwanghye on 6/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
