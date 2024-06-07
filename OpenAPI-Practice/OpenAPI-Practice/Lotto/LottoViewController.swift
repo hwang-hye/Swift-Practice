61efd188242c457cc1051390c4f0603306eae6d2
hwanghye <hwang.sine@gmail.com>
2024-06-07 22:39:43 +0900

    [Feat] Lotto API 기능 구현

--- OpenAPI-Practice/OpenAPI-Practice/Lotto/LottoViewController.swift
+++ OpenAPI-Practice/OpenAPI-Practice/Lotto/LottoViewController.swift
@@ -6,50 +6,61 @@
 //
 
 import UIKit
+import Alamofire
 import SnapKit
+
+struct Lotto: Decodable {
+    let drwNoDate: String
+    let drwtNo1: Int
+    let drwtNo2: Int
+    let drwtNo3: Int
+    let drwtNo4: Int
+    let drwtNo5: Int
+    let drwtNo6: Int
+    let bnusNo: Int
+    let drwNo: Int // 로또회차
+}
+
 
 class LottoViewController: UIViewController {
     
     let picker = UIPickerView()
+    var pickerData: [String] = []
+    var selectedValue: String?
     
     let searchTextField = UITextField()
     let infoTextLabel = UILabel()
     let infoDateLabel = UILabel()
     let infoResultLabel = UILabel()
     
-    let fistNumberResultLabel = UILabel()
-    let secondNumberResultLabel = UILabel()
-    let thirdNumberResultLabel = UILabel()
-    let fourthNumberResultLabel = UILabel()
-    let fifthNumberResultLabel = UILabel()
-    let sixthNumberResultLabel = UILabel()
-    let seventhNumberResultLabel = UILabel()
-    
-    let plusTextLabel = UILabel()
     let bonusTextLabel = UILabel()
     
     let lottoBallStackView = UIStackView()
     var lottoBalls: [UITextField] = []
     
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //        picker.delegate = self
-        //        picker.dataSource = self
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissPicker))
+        view.addGestureRecognizer(tapGesture)
+
+        
         searchTextField.inputView = picker
+        picker.delegate = self
+        picker.dataSource = self
+        searchTextField.delegate = self
         
         configureHierarchy()
         configureLayout()
         configureUI()
-        // callRequest()
+        configurePickerData()
         
+        // lottoBall UI
         for _ in 0..<8 {
             let lottoBall = UITextField()
             
-            lottoBall.text = "00"
+            lottoBall.text = ""
             lottoBall.textAlignment = .center
             lottoBall.backgroundColor = .orange
             lottoBall.font = .systemFont(ofSize: 16, weight: .bold)
@@ -84,20 +95,46 @@ class LottoViewController: UIViewController {
         }
     }
     
+    func callRequest(_ value: String?) {
+        
+        guard value != nil else { return }
+        
+        let url = "\(APIURL.lottoURL)\(searchTextField.text!)"
+        
+        AF.request(url).responseDecodable(of: Lotto.self) { response in
+            switch response.result {
+            case .success(let value):
+                print(value)
+                self.infoDateLabel.text = "\(value.drwNoDate) 추첨"
+                self.infoResultLabel.text = "\(value.drwNo)회 당첨결과"
+                
+                // lottoBall 번호를 textField에 할당
+                let numbers = [value.drwtNo1, value.drwtNo2, value.drwtNo3, value.drwtNo4, value.drwtNo5, value.drwtNo6, -1, value.bnusNo]
+                
+                for (index, lottoBall) in self.lottoBalls.enumerated() {
+                    if index == 6 {
+                        // "+" 표시를 유지
+                        lottoBall.text = "+"
+                    } else {
+                        lottoBall.text = "\(numbers[index])"
+                    }
+                }
+                
+            case .failure(let error):
+                self.searchTextField.placeholder = "올바른 회차를 입력하세요"
+            }
+        }
+    }
+    
+    
+    @objc func dismissPicker() { view.endEditing(true) }
+    
     
     func configureHierarchy() {
-        view.addSubview(picker)
         view.addSubview(searchTextField)
         view.addSubview(infoTextLabel)
         view.addSubview(infoDateLabel)
         view.addSubview(infoResultLabel)
-        view.addSubview(fistNumberResultLabel)
-        view.addSubview(secondNumberResultLabel)
-        view.addSubview(thirdNumberResultLabel)
-        view.addSubview(fourthNumberResultLabel)
-        view.addSubview(sixthNumberResultLabel)
-        view.addSubview(seventhNumberResultLabel)
-        view.addSubview(plusTextLabel)
         view.addSubview(bonusTextLabel)
         view.addSubview(lottoBallStackView)
     }
@@ -139,25 +176,57 @@ class LottoViewController: UIViewController {
         
         infoTextLabel.text = "당첨번호 안내"
         
-        infoDateLabel.text = "2020-05-30 추첨"
         infoDateLabel.textColor = .gray
         
-        infoResultLabel.text = "913회 당첨결과"
+        infoResultLabel.text = "당첨결과"
         infoResultLabel.textAlignment = .center
         infoResultLabel.font = UIFont.systemFont(ofSize: 30, weight: .semibold)
-        
-        fistNumberResultLabel.text = "6"
-        fistNumberResultLabel.backgroundColor = .yellow
-        
-        secondNumberResultLabel.text = "14"
-        secondNumberResultLabel.backgroundColor = .cyan
-        
-        thirdNumberResultLabel.text = "16"
-        thirdNumberResultLabel.backgroundColor = .cyan
         
         lottoBallStackView.axis = .horizontal
         lottoBallStackView.alignment = .fill
         lottoBallStackView.distribution = .fillEqually
         lottoBallStackView.spacing = 10
+    }
+    
+    func configurePickerData() {
+        let range = (1...1122).reversed()
+        pickerData = range.map { String($0) }
+        picker.reloadAllComponents()
+        
+        // PickerView의 초기 선택 값을 설정
+        searchTextField.text = pickerData[0]
+        selectedValue = pickerData[0]
+        
+        callRequest(selectedValue)
+    }
+}
+
+
+extension LottoViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        return true
+    }
+}
+
+
+extension LottoViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        searchTextField.text = pickerData[row]
+        selectedValue = pickerData[row]
+        callRequest(selectedValue)
+        print("select", component, row)
     }
 }
