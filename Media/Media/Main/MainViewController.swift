4648718d874824d5356da71a630cec30a56c8f03
hwanghye <hwang.sine@gmail.com>
2024-06-10 23:30:38 +0900

    [Feat] Media Interface 정의

---
+++ Media/Media/Main/MainViewController.swift
@@ -0,0 +1,66 @@
+//
+//  MainViewController.swift
+//  Media
+//
+//  Created by hwanghye on 6/10/24.
+//
+
+import UIKit
+
+struct MovieTrend: Decodable {
+    let page: String
+    let results: [Movie]
+}
+
+struct Movie: Decodable {
+    let backdropPath: String
+    let id: Int
+    let originalTitle, overview, posterPath, mediaType: String
+    let adult: Bool
+    let title, originalLanguage: String
+    let genreIDS: [Int]
+    let popularity: Double
+    let releaseDate: String
+    let video: Bool
+    let voteAverage: Double
+    let voteCount: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case backdropPath = "backdrop_path"
+        case id
+        case originalTitle = "original_title"
+        case overview
+        case posterPath = "poster_path"
+        case mediaType = "media_type"
+        case adult, title
+        case originalLanguage = "original_language"
+        case genreIDS = "genre_ids"
+        case popularity
+        case releaseDate = "release_date"
+        case video
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+    }
+}
+
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
